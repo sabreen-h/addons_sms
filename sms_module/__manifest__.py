ca9bc0ef7e796b0def47182e6733c75fee263406
Sabreen Hassan <sabreenhassan31@gmail.com>
2024-07-08 16:03:29 +0300

    [ADD] sms_module: add security groups and permissions Create and configure security roles and permissions in the SMS module for the following user roles: Administrator, Registrar, Teacher, Student, Parent/Guardian, and a new role called ReadOnly. The ReadOnly role should be a base role with read-only access to all models in the system.
    Ref#:1189

--- sms_module/__manifest__.py
+++ sms_module/__manifest__.py
@@ -23,7 +23,8 @@ This system will manage student information, courses, enrollments, grades, atten
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/groups.xml',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
